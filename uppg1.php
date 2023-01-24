f6dc289ab528b54c15a8e016827ef38db283c24c
AmmarCR77 <114470664+AmmarCR77@users.noreply.github.com>
2023-01-24 13:02:51 +0100

    upppgift 1

--- uppg1.php
+++ uppg1.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="<sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logaritmer</title>
+</head>
+<body>
 
-?>
+    <?php
+    function sum($a,$b){
+        return $a+$b;
+    }
+    echo sum(2, 5);
+    ?>  
+
+</body>
+</html>
